d3ed02ca12c7e0c28eb33649a8144a3ad49b4586
ecetera <bartula.marek@gmail.com>
2023-02-16 22:02:50 +0000

    dotenv setup

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.listen(3000, (req, res) => {
-  console.log('hi');
-});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
